9a373e68755ee9c248125d0b580e3f34191513a5
Alexandra <oleksandrapas@gmail.com>
2021-01-07 23:17:03 +0100

    old interceptors removed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
+import { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <BurgerBuilder></BurgerBuilder>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder></BurgerBuilder>
+        </Layout>
+      </div>
+    );
+  }
+
 }
 
 export default App;
