264e3e9007c619f8afc6d76fd4a4b1e2232f201c
Alexandra <oleksandrapas@gmail.com>
2021-01-16 14:52:57 +0100

    ContactData was adjusted to get data from redux state

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
@@ -100,7 +102,7 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
         }
@@ -123,11 +125,11 @@ class ContactData extends Component {
         }
 
         if (rules.minLegth) {
-            isValid = value.length >=rules.minLegth && isValid;
+            isValid = value.length >= rules.minLegth && isValid;
         }
 
         if (rules.maxLength) {
-            isValid = value.length >=rules.maxLength && isValid;
+            isValid = value.length >= rules.maxLength && isValid;
         }
 
         return isValid;
@@ -188,4 +190,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
