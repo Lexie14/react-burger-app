f8e3e4dabe83ad7470f878577bc14e7b43e82f49
Alexandra <oleksandrapas@gmail.com>
2021-01-18 10:58:37 +0100

    signin implemented

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -29,7 +29,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_d7A5qwlx3vgmjFFAjR_vqAVqXtCrhOk', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_d7A5qwlx3vgmjFFAjR_vqAVqXtCrhOk';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC_d7A5qwlx3vgmjFFAjR_vqAVqXtCrhOk';
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response));
